8ab5ed4b170bfb2134e002fd9f5441c035287f00
Merili <meriliasu@gmail.com>
2014-10-08 08:54:07 +0300

    Initial commit
    Initial commit

---
+++ components/html-head.php
@@ -0,0 +1,5 @@
+<head>
+<meta charset="UTF-8">
+<title>2. periood 2. harjutus</title>
+<link rel='stylesheet' href='stylesheets/main.css'>
+</head>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<?php include(components/html-head.php); ?>
+</head>
+<body>
+	 <h2></h2>
+		 <?php 
+		 ?>
+
+</body>
+</html>
+	
