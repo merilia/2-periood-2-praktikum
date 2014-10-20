1f99d93253666fcb4ceb38fd883b6442f9a700fd
Merili <meriliasu@gmail.com>
2014-10-20 15:11:00 +0300

    Should be working now
    Should be working now

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-<?php
-  redirect_to(header);
-?>
+<?php require "components/site-functions.php"; ?>
+<?php redirect_to('new_page.php'); ?>
 <!DOCTYPE HTML>
 <html>
 <head>
-	<?php require("components/site-functions.php"); ?>
 	<?php include("components/html-head.php"); ?>
 </head>
 <body>
